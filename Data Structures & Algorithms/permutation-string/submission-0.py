667bbf354f710e930468ebc651659626049b923d
shalszzz <shalini2410399@ssn.edu.in>
2026-07-04 13:18:26 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str):
+        def make_count(s):
+            d = {}
+            for ch in s:
+                if ch not in d:
+                    d[ch] = 1
+                else:
+                    d[ch] += 1
+            return d
+
+        d1 = make_count(s1)
+        d = d1.copy()
+        left = 0
+
+        for right in range(len(s2)):
+
+            # Character not in s1 -> reset window
+            if s2[right] not in d1:
+                d = d1.copy()
+                left = right + 1
+                continue
+
+            d[s2[right]] -= 1
+
+            while d[s2[right]] < 0:
+                d[s2[left]] += 1
+                left += 1
+
+            # Check if all counts are zero
+            if right - left + 1 == len(s1) and all(v == 0 for v in d.values()):
+                return True
+
+        return False
